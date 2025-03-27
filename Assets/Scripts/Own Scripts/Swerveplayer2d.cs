2d3a865b6e24ad80505d26d1ee4fc3a1fe43308b
Zaydan_Shakil <s4402605@glos.ac.uk>
2025-03-27 12:38:14 +0000

    Forgot what I changed
    Probably just quality of life 💩

--- Assets/Scripts/Own Scripts/Swerveplayer2d.cs
+++ Assets/Scripts/Own Scripts/Swerveplayer2d.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Swerveplayer2d : MonoBehaviour
 {
-    public bool AllowJumping = true; // Still retains jumping functionality
-    public float JumpPower = 1;      // Jump power
-    public float groundedOffset = -0.14f; // Offset to check ground collision
-    public LayerMask GroundLayerMask;     // Ground collision layer mask
-
     private Rigidbody2D rb;
-    private bool isGrounded;
     private Vector2 spherePos = Vector2.zero;
 
     private void Awake()
