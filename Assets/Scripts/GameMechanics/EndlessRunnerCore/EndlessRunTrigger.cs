6daebdc060cf8845b500584ae80cb71e2b89b197
Zaydan_Shakil <s4402605@glos.ac.uk>
2025-04-03 13:11:41 +0100

    Platform move and points

--- Assets/Scripts/GameMechanics/EndlessRunnerCore/EndlessRunTrigger.cs
+++ Assets/Scripts/GameMechanics/EndlessRunnerCore/EndlessRunTrigger.cs
@@ -17,7 +17,7 @@ public class EndlessRunTrigger : MonoBehaviour
 
 
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (Tags.Contains(other.tag) || Tags.Length == 0)
         {
